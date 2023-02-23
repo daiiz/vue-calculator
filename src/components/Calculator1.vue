2ca7c8b213f0b1648472d24ff0068c3a3e4c9f87
daiiz <da.iz.mg@gmail.com>
2023-02-24 00:52:25 +0900

    a

--- src/components/Calculator1.vue
+++ src/components/Calculator1.vue
@@ -1,3 +1,21 @@
+<script>
+export default {
+  name: "Calculator1",
+
+  data() {
+    return {
+      currentValue: ""
+    };
+  },
+
+  methods: {
+    click(e) {
+      console.log(e);
+    }
+  }
+};
+</script>
+
 <template>
   <div class="calc-container">
     <div class="calc-title">シンプルな電卓</div>
@@ -6,28 +24,28 @@
         <input readonly placeholder="0" />
       </div>
       <div class="calc-row">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>+</button>
+        <button @click="e => click(1)">1</button>
+        <button @click="e => click(2)">2</button>
+        <button @click="e => click(3)">3</button>
+        <button @click="e => click('+')">+</button>
       </div>
       <div class="calc-row">
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>-</button>
+        <button @click="e => click(4)">4</button>
+        <button @click="e => click(5)">5</button>
+        <button @click="e => click(6)">6</button>
+        <button @click="e => click('-')">-</button>
       </div>
       <div class="calc-row">
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>*</button>
+        <button @click="e => click(7)">7</button>
+        <button @click="e => click(8)">8</button>
+        <button @click="e => click(9)">9</button>
+        <button @click="e => click('*')">*</button>
       </div>
       <div class="calc-row">
-        <button>0</button>
-        <button>.</button>
-        <button>=</button>
-        <button>/</button>
+        <button @click="e => click(0)">0</button>
+        <button @click="e => click('.')">.</button>
+        <button @click="e => click('=')">=</button>
+        <button @click="e => click('/')">/</button>
       </div>
     </div>
   </div>
