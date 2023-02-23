00d9fbdeac5f4834d4b2a2c61136413a8a69b931
daiiz <da.iz.mg@gmail.com>
2023-02-24 05:54:36 +0900

    fix

--- config/index.js
+++ config/index.js
@@ -48,8 +48,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, "../docs"),
-    assetsSubDirectory: "static",
-    assetsPublicPath: "/",
+    assetsSubDirectory: "./static",
+    assetsPublicPath: "./",
 
     /**
      * Source Maps
